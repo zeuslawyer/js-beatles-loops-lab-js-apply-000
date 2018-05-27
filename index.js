91a4b71d263219bad82e4eb090e8f69ec7d436e3
zubin.pratap@gmail.com <zubin.pratap@gmail.com>
2018-05-27 08:13:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr =[];
-  for(var c = 0; c < musicians.length; c++) {
+  let c = 0;
+  for(c ; c < musicians.length; c++) {
     let str = `${musicicians[c]} plays ${instruments[c]}`;
     arr.push(str);
   }
