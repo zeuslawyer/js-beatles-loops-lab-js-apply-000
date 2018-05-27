26ab09dd4b3ddac239dd41489fb29fd7abc854b7
zubin.pratap@gmail.com <zubin.pratap@gmail.com>
2018-05-27 08:10:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array =[];
+  for (c = 0; c < musicians.length, c++) {
+    let str = `${musicicians[c]} plays ${instruments[c]}`;
+    array.push(str);
+  }
+  return array;
+}
